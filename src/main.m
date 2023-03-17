fbe256e04dc1d5132c5edda9b8ae3d02e923dd08
Sancho Amaral Simões <ssimoes@ipn.pt>
2023-03-17 11:08:09 +0000

    MASTER: Fix labels formatting and add some fixes.

--- src/main.m
+++ src/main.m
@@ -16,9 +16,12 @@ N_TOP_RANKED_FEATURES = 20;
 % Load data
 csv_data = readtable(PATH_FEATURES);
 col_names = csv_data.Properties.VariableNames(2:end - 1);
+for i=(1:size(col_names, 2))
+    col_names{i} = strrep(string(col_names{i}), "_", "-"); % Replace underscores to prevent bad format in plots
+end
 
 % Define a cell array of the target labels
-target_labels = table2cell(unique(csv_data(:, end)));
+target_labels = table2cell(unique(csv_data(:, end))); % Discard first column (file name) and last column (label)
 
 % Use the ismember function to convert the target labels to numerical format
 target_num = zeros(size(csv_data, 1), 1);
@@ -28,6 +31,7 @@ end
 
 % Kruscal wallis
 features = table2array(csv_data(:, 2:end - 1));
+features = normalize(features, 1, "range");
 features = to_data_struct(features, target_num);
 [features_h, features_idx] = rank_kruskal_wallis(features);
 
@@ -36,21 +40,25 @@ fprintf("-------Top %d KW ranked features-------\n", N_TOP_RANKED_FEATURES)
 for i=(1:N_TOP_RANKED_FEATURES)
     fprintf("%d - %s - H = %.3f\n", i, col_names{features_idx(i)}, features_h(i));
 end
+features.X = features.X(:, features_idx);
 
 figure;
-plot( (1:features.dim), features_h);
+scatter((1:features.dim), features_h);
+xlabel(col_names{features_idx(i)});
 % End of Kruscal wallis
 
 % PCA
-[coeff, score, latent] = pca_data(features); % Eu meti isto em função, mas nem deve ser necessário, mas tou a normalizar la dentro para testar so
+[coeff, score, latent] = pca_data(features);
 
-figure;plot(latent,'o-.')
+figure;
+plot(latent,'o-.')
 xlabel('principal component')
 ylabel('eig. value')
 
-total_variance=sum(latent); % Total variance
+total_variance = sum(latent); % Total variance
 
-figure;plot(cumsum(latent)./total_variance*100,'o-')
+figure;
+plot(cumsum(latent) ./ total_variance * 100, 'o-')
 xlabel('Principal component')
 ylabel('% of variance')
 

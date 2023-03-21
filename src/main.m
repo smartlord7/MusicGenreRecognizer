dd1b7788a3a1399e5d514264a227fff26355ba28
Sancho Amaral Simões <ssimoes@ipn.pt>
2023-03-21 18:46:57 +0000

    MASTER: Update images and output generation to excel.

--- src/main.m
+++ src/main.m
@@ -2,6 +2,9 @@
 close all
 clear
 
+% Make this program reproducible
+rng(0)
+
 % Constants
 DIRECTORY_DATA = "../data/";
 EXTENSION_FEATURES = ".csv";
@@ -41,7 +44,11 @@ features = table2array(csv_data(:, 2:end - 1));
 features = to_data_struct(features, target_num);
 
 % PCA
+results_table = cell(size(FUNCTIONS_NORMALIZATION, 2) * ...
+    size(FUNCTIONS_DISTANCES, 2) * ...
+    size(target_labels, 2), 8);
 
+count = 1;
 for i=(1:size(FUNCTIONS_NORMALIZATION, 2))
     norm_function = FUNCTIONS_NORMALIZATION(i);
     fprintf("Normalizing using '%s'...\n", norm_function);
@@ -56,34 +63,34 @@ for i=(1:size(FUNCTIONS_NORMALIZATION, 2))
     features_.dim = size(proj.X, 1);
     
 
-%     figure;
-%     plot(eigenValues, 'o-.');
-%     xlabel('Principal component');
-%     ylabel('Eigen value');
-%     file_path = PATH_PLOT_IMAGES + "pca_eigen_values_" + norm_function + EXTENSION_IMG;
-%     save_img(file_path);
+    figure;
+    plot(eigenValues, 'o-.');
+    xlabel('Principal component');
+    ylabel('Eigen value');
+    file_path = PATH_PLOT_IMAGES + "pca_eigen_values_" + norm_function + EXTENSION_IMG;
+    save_img(file_path);
     
-%     figure;
-%     plot(individual_variance, 'o-');
-%     xlabel('Principal component');
-%     ylabel('% of variance');
-%     file_path = PATH_PLOT_IMAGES + "pca_variance_" + norm_function + EXTENSION_IMG;
-%     save_img(file_path);
+    figure;
+    plot(individual_variance, 'o-');
+    xlabel('Principal component');
+    ylabel('% of variance');
+    file_path = PATH_PLOT_IMAGES + "pca_variance_" + norm_function + EXTENSION_IMG;
+    save_img(file_path);
 
     % Correlation study
     
     fprintf("Calculating %d PCA features correlation matrix...\n", N_PROJECTION_FEATURES);
     corr_matrix = corrcoef(features_.X'); % Only the ones most important in PCA analysis
     
-%     figure;
-%     heatmap((1:N_PROJECTION_FEATURES), (1:N_PROJECTION_FEATURES), corr_matrix);
-%     file_path = PATH_PLOT_IMAGES + "corr_matrix_pca_" + norm_function + EXTENSION_IMG;
-%     save_img(file_path);
+    figure;
+    heatmap((1:N_PROJECTION_FEATURES), (1:N_PROJECTION_FEATURES), corr_matrix);
+    file_path = PATH_PLOT_IMAGES + "corr_matrix_pca_" + norm_function + EXTENSION_IMG;
+    save_img(file_path);
 
-%     figure;
-%     ppatterns(features_);
-%     file_path = PATH_PLOT_IMAGES + "patterns_pca" + EXTENSION_IMG;
-%     save_img(file_path);
+    figure;
+    ppatterns(features_);
+    file_path = PATH_PLOT_IMAGES + "patterns_pca" + EXTENSION_IMG;
+    save_img(file_path);
 
     % Kruskal Wallis
 
@@ -103,15 +110,15 @@ for i=(1:size(FUNCTIONS_NORMALIZATION, 2))
     features_.dim = size(features_.X, 1);
 %     test_norm(features_, col_names_);
     
-%     figure;
-%     scatter((1:size(features_h, 2)), features_h);
-%     file_path = PATH_PLOT_IMAGES + "scatter_h_kw" + EXTENSION_IMG;
-%     save_img(file_path);
+    figure;
+    scatter((1:size(features_h, 2)), features_h);
+    file_path = PATH_PLOT_IMAGES + "scatter_h_kw" + EXTENSION_IMG;
+    save_img(file_path);
 
-%     figure;
-%     ppatterns(features_);
-%     file_path = PATH_PLOT_IMAGES + "patterns_pca_kw" + EXTENSION_IMG;
-%     save_img(file_path);
+    figure;
+    ppatterns(features_);
+    file_path = PATH_PLOT_IMAGES + "patterns_pca_kw" + EXTENSION_IMG;
+    save_img(file_path);
 
     
     % End of Kruscal Wallis
@@ -120,10 +127,10 @@ for i=(1:size(FUNCTIONS_NORMALIZATION, 2))
     
     fprintf("Calculating %d top KW features correlation matrix...\n", N_TOP_DISCRIMINANT_KW_RANKED_FEATURES);
     corr_matrix = corrcoef(features_.X'); % Only 20 best
-%     figure;
-%     heatmap(col_names_, col_names_, corr_matrix);
-%     file_path = PATH_PLOT_IMAGES + "corr_matrix_kw" + EXTENSION_IMG;
-%     save_img(file_path);
+    figure;
+    heatmap(col_names_, col_names_, corr_matrix);
+    file_path = PATH_PLOT_IMAGES + "corr_matrix_kw" + EXTENSION_IMG;
+    save_img(file_path);
     
     % End of correlation study
     
@@ -142,8 +149,8 @@ for i=(1:size(FUNCTIONS_NORMALIZATION, 2))
         fprintf("%d - %s - I = %.3f\n", j, col_names_{idx(j)}, importances(j));
     end
     
-%     file_path = PATH_PLOT_IMAGES + "importance_rf" + EXTENSION_IMG;
-%     save_img(file_path);
+    file_path = PATH_PLOT_IMAGES + "importance_rf" + EXTENSION_IMG;
+    save_img(file_path);
     
     % End of RandomForest
 
@@ -181,12 +188,31 @@ for i=(1:size(FUNCTIONS_NORMALIZATION, 2))
                 "Sensitivity: %.3f\n" + ...
                 "F-measure: %.3f\n", mse, accuracy, specificity, sensitivity, f_measure);
             
+             % store the results in a cell array
+                results_table{count, 1} = FUNCTIONS_NORMALIZATION(i);
+                results_table{count, 2} = FUNCTIONS_DISTANCES(k);
+                results_table{count, 3} = string(genre);
+                results_table{count, 4} = mse;
+                results_table{count, 5} = accuracy;
+                results_table{count, 6} = specificity;
+                results_table{count, 7} = sensitivity;
+                results_table{count, 8} = f_measure;
+
+                count = count + 1;        
         end
 
         % Reset labels
         data_mdc.y = features_.y;
     end
 end
+
+% convert the cell array to a table
+header = {'Normalization function', 'Distance function', ...
+'Class', 'MSE', 'Accuracy', 'Specificity', 'Sensitivity', 'F-measure'};
+results_table = cell2table(results_table, 'VariableNames', header);
+
+% write the table to an Excel file
+writetable(results_table, 'results.xlsx');
 
 % End of PCA
 
